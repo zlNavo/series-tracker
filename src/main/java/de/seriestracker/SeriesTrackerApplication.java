0165a5ec34d9147fd958bfbe498110c4ccad59b4
zlNavo <asator0412@gmail.com>
2025-03-15 16:52:32 +0100

    Docker compose file for mariadb, spring boot properties set to connect to db

---
+++ src/main/java/de/seriestracker/SeriesTrackerApplication.java
@@ -0,0 +1,13 @@
+package de.seriestracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeriesTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeriesTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/seriestracker/SeriesTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.seriestracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeriesTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
